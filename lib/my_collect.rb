a5cf1ff9c3297b7627976439a61734c02ef9be13
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-30 05:02:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
 def collect ("Tim Jones", "Tom Smith", "Jim Campagno")
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
+ i = 0
+ collection = []
+ while i < array.length
+   collection << yield(array[i])
+   i += 1
+ end
+ collection
 end
